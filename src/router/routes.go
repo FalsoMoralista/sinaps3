132d5b218126110e54b0a2fe12841c65573006ff
luciano <lucianoadfilho@gmail.com>
2019-06-29 22:23:34 -0300

    testing

--- src/router/routes.go
+++ src/router/routes.go
@@ -2,41 +2,45 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"model"
 	"net/http"
+	"os"
 )
 
-
-func main(){
+func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/",home)
-	router.HandleFunc("/teste",GetProcedimentos)
-	log.Fatal(http.ListenAndServe(":8000", router))
+	router.HandleFunc("/procedimentos-disponiveis", ProcedimentosDisponiveis)
+	fmt.Println("listening...")
+	log.Fatal(http.ListenAndServe(GetPort(), router))
 }
 
-func home(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type","text/html")
-	http.ServeFile(w,req,"www/index.html")
-}
-
-func GetProcedimentos(w http.ResponseWriter, req *http.Request) {
+func ProcedimentosDisponiveis(w http.ResponseWriter, req *http.Request) {
 	procedimentos, err := model.GetProcedimentos()
 	if err == nil {
 		var nao_agendados []model.Procedimento
-		for _,procedimento := range procedimentos{
+		for _, procedimento := range procedimentos {
 			if procedimento.Paciente_id == "" {
 				nao_agendados = append(nao_agendados, procedimento)
 			}
 		}
-		w.Header().Set("Content-Type","application/json")
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		bArray,_ := json.Marshal(nao_agendados)
+		bArray, _ := json.Marshal(nao_agendados)
 		w.Write(bArray)
-	}else{
+	} else {
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
 
-
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
+}
