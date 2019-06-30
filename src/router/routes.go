68233fc547b726a29c7c2146bc4b9135779ca41d
luciano <lucianoadfilho@gmail.com>
2019-06-30 08:56:52 -0300

    updating

--- src/router/routes.go
+++ src/router/routes.go
@@ -12,13 +12,24 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+	root := http.FileServer(http.Dir("www/startbootstrap-landing-page-gh-pages/"))
+	css := http.FileServer(http.Dir("www/startbootstrap-landing-page-gh-pages/css"))
+ 	router.Handle("/",root)
+	router.Handle("/index.html",root)
+	router.Handle("/css",css)
 
+	router.HandleFunc("/paciente.html", Paciente)
+	router.HandleFunc("/medico.html", Medico)
+	router.HandleFunc("/paciente", Paciente)
+	router.HandleFunc("/medico", Medico)
 	router.HandleFunc("/procedimentos/disponiveis", ProcedimentosDisponiveis)
-	router.HandleFunc("/paciente", Pacientes)
-
+	router.HandleFunc("/img/sinaps_logo1.png",logoSinaps)
+	router.HandleFunc("/vendor/jquery/jquery.min.js", jquery)
+	router.HandleFunc("/vendor/bootstrap/js/bootstrap.bundle.min.js", bootstrap_js)
 	router.HandleFunc("/vendor/bootstrap/css/bootstrap.min.css", bootstrap)
 	router.HandleFunc("/vendor/fontawesome-free/css/all.min.css", all)
 	router.HandleFunc("/vendor/simple-line-icons/css/simple-line-icons.css", line_icons)
+	router.HandleFunc("/vendor/simple-line-icons/fonts/Simple-Line-Icons.woff2?v=2.4.0",simple_icons)
 	router.HandleFunc("/css/landing-page.min.css", landing_page)
 	router.HandleFunc("/js/handy-collapse.js", handy_collapse)
 	router.HandleFunc("/js/index.js", index_js)
@@ -27,6 +38,39 @@ func main() {
 	fmt.Println("listening ...")
 	log.Fatal(http.ListenAndServe(GetPort(), router))
 }
+
+
+func simple_icons(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/font")
+	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/simple-line-icons/fonts/Simple-Line-Icons.woff2?v=2.4.0")
+}
+
+func jquery(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/vendor/jquery/jquery.min.js")
+}
+
+func bootstrap_js(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/vendor/bootstrap/js/bootstrap.bundle.min.js")
+}
+func home(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type","text/html")
+	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/index.html")
+}
+
+
+func logoSinaps(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type","image/png")
+	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/sinaps_logo1.png")
+}
+
+func Medico(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type","text/html")
+	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/medico.html")
+}
+
+
 
 func index_js(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type","application/javascript")
@@ -65,7 +109,7 @@ func bootstrap(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/vendor/bootstrap/css/bootstrap.min.css")
 }
 
-func Pacientes(w http.ResponseWriter, req *http.Request) {
+func Paciente(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type","text/html")
 	http.ServeFile(w,req,"www/startbootstrap-landing-page-gh-pages/paciente.html")
 }
